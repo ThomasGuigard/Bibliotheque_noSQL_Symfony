5a9b4911833c85c9ce4c2ce3ad42d1db204266d9
ThomasGuigard <thomasguigard@gmail.com>
2017-06-18 19:56:01 +0200

    Ajout d'une fonctionnalité pour lister les livres et d'une autre pour les supprimer

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Document\Livre;
 
 class DefaultController extends Controller
 {
@@ -14,13 +15,27 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
 
+  /*    $product = new Livre();
+      $product->setTitre('Titres');
+      $product->setIsbn('Isbnr');
+      $product->setPret('Prear');
+      $product->setThematiques('A Thematique');
+      $product->setEtat('A Etar');
+      $product->setAuteur('Auteu');
+
+     $dm = $this->get('doctrine_mongodb')->getManager();
+     $dm->persist($product);
+     $dm->flush();*/
+
+
         $livres = $this->get('doctrine_mongodb')
         ->getRepository('AppBundle:Livre')
         ->findAll();
 
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR, 'livres' => $livres
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+             'livres' => $livres
         ]);
     }
 }
--- src/AppBundle/Controller/LivreController.php
+++ src/AppBundle/Controller/LivreController.php
@@ -1,26 +1,27 @@
 <?php
 
+
 namespace AppBundle\Controller;
 
-use Acme\StoreBundle\Document\Product;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Document\Livre;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class LivreController extends Controller{
+class LivreController extends Controller
+{
 
 
-  public function addABookAction(){
+ public function deleteBookAction($id){
 
+    $livre=$this->get('doctrine_mongodb')->getRepository('AppBundle:Livre')->find($id);
+    $dm = $this->get('doctrine_mongodb')->getManager();
+    $dm->remove($livre);
+    $dm->flush();
 
+    return $this->redirectToRoute('homepage');
   }
 
-
-  public function removeABookAction(){
-
-
-  }
-
-  public function setPretAction(){
-
-  }
 
 }
